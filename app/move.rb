fed0dd0189d301cdf4aa249437eeaa795f10bf9e
Jordan Sandberg <jordansandberg1@gmail.com>
2022-06-16 21:02:42 -0700

    fix bug

--- app/move.rb
+++ app/move.rb
@@ -6,17 +6,18 @@ require_relative 'graph/board'
 # Valid moves are "up", "down", "left", or "right".
 
 def move(game)
-  paths = Graph::Board.new(game[:board].merge({ you: game[:you] }))
-  path = paths.search if paths
+  path = Graph::Board.new(game[:board].merge({ you: game[:you] })).search[1]
   # pp "#{game[:you][:head][:x]} #{game[:you][:head][:y]}"
   # pp "#{path.x} #{path.y}"
   # pp path
 
   # Choose a random direction to move in
-  possible_moves = %w[up down left right]
-  move = possible_moves.sample
-  # puts "MOVE: #{move}"
-  { move: path.nil? ? move : get_move(game[:you][:head], path) }
+  # possible_moves = %w[up down left right]
+  # move = possible_moves.sample
+  move = get_move(game[:you][:head], path)
+  puts "MOVE: #{move}"
+
+  { move: move }
 end
 
 def get_move(me, path)
