482be7417605124f06f45f2d49b53d9dca1135f7
Jordan Sandberg <jordansandberg1@gmail.com>
2022-06-19 14:36:11 -0700

    clean a bunch of code

--- app/graph/board.rb
+++ app/graph/board.rb
@@ -7,15 +7,15 @@ module Graph
   class Board < AStar
     attr_reader :graph, :start
 
-    def initialize(board_json)
-      graph = build_graph(board_json)
-      @start = find_my_head(board_json, graph)
+    def initialize(board_arr, me)
+      graph = build_graph(board_arr)
+      @start = graph.keys.select { |node| node.x == me[:head][:x] && node.y == me[:head][:y] }.first
       @end = find_closest_food(@start, graph)
       super(graph)
     end
 
-    def search(end_ovrd = nil)
-      super(@start, end_ovrd || @end)
+    def search
+      super(@start, @end)
     end
 
     def heuristic(_node, _start, _goal)
@@ -24,68 +24,18 @@ module Graph
 
     private
 
-    def build_graph(board_json)
-      dimensions, food, points_to_ignore = destructure_json(board_json)
+    def build_graph(board_arr)
+      board_nodes = board_arr.each_with_index.map do |column_arr, x|
+        column_arr.each_with_index.map do |tile, y|
+          next if tile.nil?
 
-      food_nodes = food.map { |f| BoardNode.new(:food, f[:x], f[:y]) }
-
-      x_range = (0..dimensions[:width])
-      y_range = (0..dimensions[:height])
-      board_nodes = []
-      x_range.each do |x|
-        y_range.each do |y|
-          ignore = false
-          (points_to_ignore + food).flatten.each do |point|
-            ignore = (point[:x] == x && point[:y] == y)
-            break if ignore
-          end
-          board_nodes.append(BoardNode.new(:board, x, y)) unless ignore
+          BoardNode.new(tile, x, y)
         end
+      end.flatten.compact
+
+      board_nodes.to_h do |node|
+        [node, board_nodes.select { |x| x.neighbour?(node) }]
       end
-
-      graph = {}
-      # for each nodes = food_nodes + board_no: node
-      me = board_json[:you]
-      nodes = (food_nodes + board_nodes).flatten.append(BoardNode.new(:board, me[:head][:x], me[:head][:y]))
-      nodes.each do |node|
-        graph[node] = nodes.select do |subject|
-          x_neighbour = (subject.x == node.x - 1 || subject.x == node.x + 1) && subject.y == node.y
-          y_neighbour = (subject.y == node.y - 1 || subject.y == node.y + 1) && subject.x == node.x
-          x_neighbour || y_neighbour
-        end
-      end
-
-      graph
-    end
-
-    def destructure_json(board_json)
-      dimensions = board_json.slice(:height, :width)
-      food = board_json[:food]
-      snakes = board_json[:snakes].map { |snake| snake[:body] }.flatten
-      hazards = board_json[:hazards]
-
-      if board_json[:snakes].size > 1
-        me = board_json[:you]
-        my_length = me[:body].size
-        longer_snakes = snakes.select { |snake| snake[:body].length >= my_length && snake[:head] != me[:head] }
-        longer_snake_moves = longer_snakes.map do |_long_snake|
-          [
-            { x: longer_snakes[:head][:x] + 1, y: longer_snakes[:head][:y] },
-            { x: longer_snakes[:head][:x] - 1, y: longer_snakes[:head][:y] },
-            { x: longer_snakes[:head][:x], y: longer_snakes[:head][:y] + 1 },
-            { x: longer_snakes[:head][:x], y: longer_snakes[:head][:y] - 1 }
-          ]
-        end
-      end
-
-      [dimensions, food, (snakes + hazards + [longer_snake_moves]).flatten.uniq.compact]
-    end
-
-    def find_my_head(board_json, graph)
-      me = board_json[:you]
-      x = me[:head][:x]
-      y = me[:head][:y]
-      graph.keys.select { |node| node.x == x && node.y == y }.first
     end
 
     def distance(p1, p2)
@@ -94,9 +44,6 @@ module Graph
 
     def find_closest_food(start, graph)
       food = graph.keys.select { |node| node.type == :food }
-
-      return graph.keys.min_by { |food| distance(start, food) } if food.empty?
-
       food.min_by { |f| distance(start, f) }
     end
   end
